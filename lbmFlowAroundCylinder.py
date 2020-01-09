ccd2894a1fa70f3c58086033001f919d20473880
Jonathan Viquerat <jonathan.viquerat@mines-paristech.fr>
2020-01-09 09:07:37 +0100

    Reformatting code

--- lbmFlowAroundCylinder.py
+++ lbmFlowAroundCylinder.py
@@ -1,33 +1,41 @@
-#!/usr/bin/python
-# Copyright (C) 2013 FlowKit Ltd, Lausanne, Switzerland
-# E-mail contact: contact@flowkit.com
-#
-# This program is free software: you can redistribute it and/or
-# modify it under the terms of the GNU General Public License, either
-# version 3 of the License, or (at your option) any later version.
+# Generic imports
+from numpy        import *
+from numpy.linalg import *
+import matplotlib.pyplot as plt
+from matplotlib import cm
 
-#
-# 2D flow around a cylinder
-#
+### Flow parameters
+maxIter = 20000     # total nb of iterations
+Re      = 1000.0    # Re number
+nx      = 520
+ny      = 180
+ly      = ny-1.0    # lattice dimension
+q       = 9         # lattice population
+cx      = nx/4      # cylinder coordinates
+cy      = ny/2      # cylinder coordinates
+r       = ny/9;     # cylinder radius
+uLB     = 0.04      # velocity in lattice units
+nulb    = uLB*r/Re
+omega   = 1.0/(3.*nulb+0.5) # relaxation parameter.
 
-from numpy import *; from numpy.linalg import *
-import matplotlib.pyplot as plt; from matplotlib import cm
-###### Flow definition #########################################################
-maxIter = 20000 # Total number of time iterations.
-Re      = 1000.0  # Reynolds number.
-nx = 520; ny = 180; ly=ny-1.0; q = 9 # Lattice dimensions and populations.
-cx = nx/4; cy=ny/2; r=ny/9;          # Coordinates of the cylinder.
-uLB     = 0.04                       # Velocity in lattice units.
-nulb    = uLB*r/Re; omega = 1.0 / (3.*nulb+0.5); # Relaxation parameter.
+### Lattice definition
 
-###### Lattice Constants #######################################################
-c = array([(x,y) for x in [0,-1,1] for y in [0,-1,1]]) # Lattice velocities.
-t = 1./36. * ones(q)                                   # Lattice weights.
-t[asarray([norm(ci)<1.1 for ci in c])] = 1./9.; t[0] = 4./9.
-noslip = [c.tolist().index((-c[i]).tolist()) for i in range(q)] 
+# Set lattice velocities
+c = array([(x,y) for x in [0,-1,1] for y in [0,-1,1]]) # lattice velocities
+
+# Set lattice weights
+t                                      = ones(q);
+t[asarray([norm(ci)<1.1 for ci in c])] = 1./9.  # cardinal       values
+t[asarray([norm(ci)>1.1 for ci in c])] = 1./36. # extra-cardinal values
+t[0]                                   = 4./9.  # central        value
+
+
+noslip = [c.tolist().index((-c[i]).tolist()) for i in range(q)]
 i1 = arange(q)[asarray([ci[0]<0  for ci in c])] # Unknown on right wall.
 i2 = arange(q)[asarray([ci[0]==0 for ci in c])] # Vertical middle.
 i3 = arange(q)[asarray([ci[0]>0  for ci in c])] # Unknown on left wall.
+
+print(noslip)
 
 ###### Function Definitions ####################################################
 sumpop = lambda fin: sum(fin,axis=0) # Helper function for density computation.
@@ -40,8 +48,9 @@ def equilibrium(rho,u):              # Equilibrium distribution function.
 
 ###### Setup: cylindrical obstacle and velocity inlet with perturbation ########
 obstacle = fromfunction(lambda x,y: (x-cx)**2+(y-cy)**2<r**2, (nx,ny))
-vel = fromfunction(lambda d,x,y: (1-d)*uLB*(1.0+1e-4*sin(y/ly*2*pi)),(2,nx,ny))
-feq = equilibrium(1.0,vel); fin = feq.copy()
+vel      = fromfunction(lambda d,x,y: (1-d)*uLB*(1.0+1e-4*sin(y/ly*2*pi)),(2,nx,ny))
+feq      = equilibrium(1.0,vel)
+fin      = feq.copy()
 
 ###### Main time loop ##########################################################
 for time in range(maxIter):
@@ -60,6 +69,6 @@ for time in range(maxIter):
         fin[i,:,:] = roll(roll(fout[i,:,:],c[i,0],axis=0),c[i,1],axis=1)
  
     if (time%100==0): # Visualization
-        plt.clf(); plt.imshow(sqrt(u[0]**2+u[1]**2).transpose(),cmap=cm.Reds)
+        plt.clf()
+        plt.imshow(sqrt(u[0]**2+u[1]**2).transpose(),cmap=cm.Reds)
         plt.savefig("vel."+str(time/100).zfill(4)+".png")
-
