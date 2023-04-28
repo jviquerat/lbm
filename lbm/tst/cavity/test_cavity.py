08461ca124101cb7e288914ddd579a0d5bd68497
jviquerat <jonathan.viquerat@mines-paristech.fr>
2023-04-28 08:46:18 +0200

    Fixed zou-he top bc for velocity

--- lbm/tst/cavity/test_cavity.py
+++ lbm/tst/cavity/test_cavity.py
@@ -22,7 +22,7 @@ def test_cavity():
 
     # Check error
     vx, uy = app.line_fields(ltc)
-    assert(abs(vx[10] - 0.12493089684236539) < 1.0e-8)
-    assert(abs(vx[50] - 0.05295104908939561) < 1.0e-8)
-    assert(abs(uy[10] + 0.05968571489630510) < 1.0e-8)
-    assert(abs(uy[50] + 0.19792323493599165) < 1.0e-8)
+    assert(abs(vx[10] - 0.12493089684236539) < 1.0e-6)
+    assert(abs(vx[50] - 0.05295104908939561) < 1.0e-6)
+    assert(abs(uy[10] + 0.05968571489630510) < 1.0e-6)
+    assert(abs(uy[50] + 0.19792323493599165) < 1.0e-6)
